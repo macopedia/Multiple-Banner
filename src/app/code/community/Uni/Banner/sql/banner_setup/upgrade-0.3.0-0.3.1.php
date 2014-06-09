7a8caba09434bdee41ee487deed3882582940ea4
Agata <a.firlejczyk@macopedia.pl>
2014-06-09 12:49:37 +0200

    Add new column - store view. Change column banner_name to identifier

--- src/app/code/community/Uni/Banner/sql/banner_setup/upgrade-0.3.0-0.3.1.php
+++ src/app/code/community/Uni/Banner/sql/banner_setup/upgrade-0.3.0-0.3.1.php
@@ -5,7 +5,7 @@ $installer = $this;
 $table = $installer->getTable('banner/banner');
 
 $installer->getConnection()->addColumn(
-        $table, 'banner_name', ' varchar(255) NOT NULL default ""'
+        $table, 'identifier', ' varchar(255) NOT NULL default ""'
     );
 
 $installer->getConnection()->addColumn(
@@ -14,6 +14,10 @@ $installer->getConnection()->addColumn(
 
 $installer->getConnection()->addColumn(
     $table, 'banner_height', ' SMALLINT( 4 ) NOT NULL DEFAULT 0'
+);
+
+$installer->getConnection()->addColumn(
+    $table, 'store', ' SMALLINT( 4 ) NOT NULL DEFAULT 1'
 );
 
 $installer->startSetup();
