b4f6f9940ff5df0b5affa1cd62ed90030f71a75f
Agata <a.firlejczyk@macopedia.pl>
2014-06-09 12:50:40 +0200

    Change banner_name to identifier

--- src/app/code/community/Uni/Banner/Block/Banner/Single.php
+++ src/app/code/community/Uni/Banner/Block/Banner/Single.php
@@ -14,7 +14,7 @@ class Uni_Banner_Block_Banner_Single extends Mage_Core_Block_Template
     public function getBanner($id)
     {
         return Mage::getModel('banner/banner')->getCollection()
-            ->addFieldToFilter('banner_name', $id)->getFirstItem();
+            ->addFieldToFilter('identifier', $id)->getFirstItem();
     }
 
     /**
