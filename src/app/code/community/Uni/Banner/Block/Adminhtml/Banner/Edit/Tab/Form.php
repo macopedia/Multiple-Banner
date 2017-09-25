1a9544d85a20d6ea720616c62a2cb12edff91f77
Adam Marcinkowski <a.marcinkowski@macopedia.pl>
2017-09-25 13:18:10 +0200

    Hide unused fields

--- src/app/code/community/Uni/Banner/Block/Adminhtml/Banner/Edit/Tab/Form.php
+++ src/app/code/community/Uni/Banner/Block/Adminhtml/Banner/Edit/Tab/Form.php
@@ -33,10 +33,10 @@ class Uni_Banner_Block_Adminhtml_Banner_Edit_Tab_Form extends Mage_Adminhtml_Blo
             'name'     => 'title',
         ));
 
-        $fieldset->addField('subtitle', 'text', array(
-            'label' => Mage::helper('banner')->__('Subtitle'),
-            'name'  => 'subtitle'
-        ));
+//        $fieldset->addField('subtitle', 'text', array(
+//            'label' => Mage::helper('banner')->__('Subtitle'),
+//            'name'  => 'subtitle'
+//        ));
 
         $fieldset->addField('identifier', 'text', array(
             'label'    => Mage::helper('banner')->__('Identifier'),
@@ -103,17 +103,17 @@ class Uni_Banner_Block_Adminhtml_Banner_Edit_Tab_Form extends Mage_Adminhtml_Blo
             ));
         }
 
-        $fieldset->addField('banner_width', 'text', array(
-            'label'    => Mage::helper('banner')->__('Banner Width [in px]'),
-            'required' => false,
-            'name'     => 'banner_width',
-        ));
-
-        $fieldset->addField('banner_height', 'text', array(
-            'label'    => Mage::helper('banner')->__('Banner Height [in px]'),
-            'required' => false,
-            'name'     => 'banner_height',
-        ));
+//        $fieldset->addField('banner_width', 'text', array(
+//            'label'    => Mage::helper('banner')->__('Banner Width [in px]'),
+//            'required' => false,
+//            'name'     => 'banner_width',
+//        ));
+//
+//        $fieldset->addField('banner_height', 'text', array(
+//            'label'    => Mage::helper('banner')->__('Banner Height [in px]'),
+//            'required' => false,
+//            'name'     => 'banner_height',
+//        ));
 
         $fieldset->addField('sort_order', 'text', array(
             'label' => Mage::helper('banner')->__('Sort Order'),
@@ -136,26 +136,26 @@ class Uni_Banner_Block_Adminhtml_Banner_Edit_Tab_Form extends Mage_Adminhtml_Blo
             ),
         ));
 
-
-        $fieldset->addField(
-            'mobile', 'select', array(
-                'name'     => 'mobile',
-                'label'    => Mage::helper('banner')->__('Mobile'),
-                'title'    => Mage::helper('banner')->__('Mobile'),
-                'disabled' => false,
-
-                'values' => array(
-                    array(
-                        'value' => 1,
-                        'label' => Mage::helper('banner')->__('Yes'),
-                    ),
-                    array(
-                        'value' => 0,
-                        'label' => Mage::helper('banner')->__('No'),
-                    ),
-                ),
-            )
-        );
+//
+//        $fieldset->addField(
+//            'mobile', 'select', array(
+//                'name'     => 'mobile',
+//                'label'    => Mage::helper('banner')->__('Mobile'),
+//                'title'    => Mage::helper('banner')->__('Mobile'),
+//                'disabled' => false,
+//
+//                'values' => array(
+//                    array(
+//                        'value' => 1,
+//                        'label' => Mage::helper('banner')->__('Yes'),
+//                    ),
+//                    array(
+//                        'value' => 0,
+//                        'label' => Mage::helper('banner')->__('No'),
+//                    ),
+//                ),
+//            )
+//        );
 
         $fieldset->addField('product_id', 'text', array(
             'label'    => Mage::helper('banner')->__('Product SKU'),
@@ -163,33 +163,33 @@ class Uni_Banner_Block_Adminhtml_Banner_Edit_Tab_Form extends Mage_Adminhtml_Blo
             'name'     => 'product_id',
         ));
 
-        $fieldset->addField('product_title', 'text', array(
-            'label' => Mage::helper('banner')->__('Product title'),
-            'name'  => 'product_title'
-        ));
+//        $fieldset->addField('product_title', 'text', array(
+//            'label' => Mage::helper('banner')->__('Product title'),
+//            'name'  => 'product_title'
+//        ));
+//
+//        $fieldset->addField('product_position', 'text', array(
+//            'label' => Mage::helper('banner')->__('Product position'),
+//            'name'  => 'product_position',
+//            'note' => 'eg. <strong> top: 20px; right: 50px; </strong> <br/> remember about semicolon!'
+//        ));
+//
+//        $fieldset->addField('badge_label', 'text', array(
+//            'label' => Mage::helper('banner')->__('Badge label'),
+//            'name'  => 'badge_label'
+//        ));
+//
+//        $fieldset->addField('badge_color', 'text', array(
+//            'label' => Mage::helper('banner')->__('Badge color'),
+//            'name'  => 'badge_color'
+//        ));
 
-        $fieldset->addField('product_position', 'text', array(
-            'label' => Mage::helper('banner')->__('Product position'),
-            'name'  => 'product_position',
-            'note' => 'eg. <strong> top: 20px; right: 50px; </strong> <br/> remember about semicolon!'
-        ));
 
-        $fieldset->addField('badge_label', 'text', array(
-            'label' => Mage::helper('banner')->__('Badge label'),
-            'name'  => 'badge_label'
-        ));
-
-        $fieldset->addField('badge_color', 'text', array(
-            'label' => Mage::helper('banner')->__('Badge color'),
-            'name'  => 'badge_color'
-        ));
-
-
-        $fieldset->addField('product_image_path', 'image', array(
-            'label'    => Mage::helper('banner')->__('Product image'),
-            'required' => false,
-            'name'     => 'product_image_path',
-        ));
+//        $fieldset->addField('product_image_path', 'image', array(
+//            'label'    => Mage::helper('banner')->__('Product image'),
+//            'required' => false,
+//            'name'     => 'product_image_path',
+//        ));
 
         $fieldset->addField('schedule_enabled', 'checkbox', array(
             'label'   => Mage::helper('banner')->__('Enable schedule'),
